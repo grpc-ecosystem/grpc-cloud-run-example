ba2543f4664861343ace7d47d6683af7f8b3f590
Richard Belleville <rbellevi@google.com>
2020-01-31 11:19:36 -0800

    Make deploy.sh language-agnostic

---
+++ deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ "$#" != "2" ]; then
+  echo "Usage: $0 GCP_PROJECT LANGUAGE" 1>&2
+  exit 1
+fi
+
+LANGUAGES="node python"
+
+if ! grep -oh "$2" 2>&1 1>/dev/null <<< "${LANGUAGES}"; then
+  echo "Unsupported language \"$2\"" 1>&2
+  echo "Supported languages: ${LANGUAGES}" 1>&2
+  exit 1
+fi
+
+
+GCP_PROJECT="$1"
+LANGUAGE="$2"
+CONTAINER_TAG="gcr.io/${GCP_PROJECT}/grpc-calculator:latest"
+
+set -x
+(cd "${LANGUAGE}"
+  docker build -t "${CONTAINER_TAG}" .
+  docker push "${CONTAINER_TAG}"
+  gcloud run deploy --image "${CONTAINER_TAG}" --platform managed
+)
